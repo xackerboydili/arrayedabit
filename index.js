f33ca1f631cb5c68ae1afa17230ef547e819c8ce
xackerboydili <isoqovdilshod47@gmail.com>
2022-11-05 23:36:52 +0500

    arrays

---
+++ index.js
@@ -0,0 +1,240 @@
+
+"1"
+
+// function getFirstValue(arr) {
+// 	return arr[0]
+// }
+// console.log(getFirstValue([4, 5, 5, 6,]))
+
+"2"
+
+// function makePair(num1, num2) {
+// 	return Array.of(num1, num2)
+// }
+// console.log(makePair(3, 5))
+"3"
+// function drop(arr, val = 1) {
+// 	return arr.slice(val)
+// }
+// console.log(drop([1, 2, 3], 0))
+"4"
+// function reverse(arr) {
+// 	return arr.reverse()
+// }
+// console.log(reverse([1, 2, 3, 4]))
+
+"5"
+// function incrementItems(arr) {
+//     return arr.map((e) => e + 1)
+// }
+// console.log(incrementItems([0, 1, 2, 3]))
+
+"6"
+// const arr = [1, 2, 3, 4, 5, 6]
+// let a = arr[0]
+// let b = arr[1]
+
+// console.log(a)
+// console.log(b)
+
+"7"
+// function getLastItem(arr) {
+//     return arr.pop()
+// }
+// console.log(getLastItem(["cat", "dog", "duck"]))
+
+"8"
+// function arrayToString(arr) {
+// 	return arr.join("")
+// }
+// console.log(arrayToString(["a", "b", "c", "d", "e", "f"]))
+
+"9"
+// function concat(arr1, arr2) {
+// 	return [...arr1, ...arr2]
+// }
+// console.log(concat([1, 3, 5], [2, 6, 8]))
+
+"10"
+// function findIndex(arr, str) {
+// 	return arr.indexOf(str)
+// }
+// console.log(findIndex(["a", "g", "y", "d"], "d"))
+
+"11"
+// function valueAt(arr, i) {
+// 	return arr.at(i)
+// }
+// console.log(valueAt([1, 2, 3, 4, 5, 6], 10 / 2))
+
+"12"
+// function search(arr, item) {
+// 	for(let i = 0; i < arr.length; i++){
+//         if(arr[i] == item){
+//             return i
+//         }
+//     }
+// }
+// console.log(search([1, 5, 3], 5))
+
+"13"
+
+// function sumArray(arr) {
+//   let  sum 	= 0
+//   for(i=0; i<arr.length; i++) {
+// 	  sum += arr[i]
+// 	}
+//   return sum
+// }
+// console.log(sumArray([1, 2, 3, 4, 5]))
+
+"14"
+
+// function search(arr, item) {
+// 	return arr.indexOf(item)
+// }
+// console.log(search([2, 4, 6, 8, 10], 8))
+
+"15"
+
+// function check(arr, el) {
+// 	return arr.includes(el) ? true : false
+// }
+// console.log(check([1, 1, 2, 1, 1], 3))
+
+"16"
+// function arrayValuesTypes(arr) {
+// 	return arr.map( (e) => typeof e)
+// }
+// console.log(arrayValuesTypes([1, 2, "null", []]))
+
+"17"
+// function fiftyThirtyTwenty(ati) {
+// 	return ` Needs:${ati * 0.5} Wants: ${ati * 0.3} Savings: ${ati * 0.2}`
+// }
+// console.log(fiftyThirtyTwenty(10000))
+
+"18"
+// function arrBetween(num1, num2, arr) {
+// 	return arr.filter((e) => e > num1 && e < num2)
+// }
+// console.log(arrBetween(3, 8, [1, 5, 95, 0, 4, 7]))
+
+"19"
+
+// function hasSameBread(arr1, arr2) {
+//     return arr1[0] === arr2[0] && arr1[2] === arr2[2];
+// }
+// console.log(hasSameBread( ["brown bread", "chicken", "brown bread"],
+// ["white bread", "chicken", "white bread"]))
+
+"20"
+// function parseArray(arr) {
+// 	return arr.map((e) => String(e))
+// }
+// console.log(parseArray([1, 2, "a", "b"]))
+
+"21"
+// function toNumberArray(arr) {
+//     return arr.map((e) => Number(e))
+// }
+// console.log(toNumberArray(["1", "3", "3.6"]))
+
+"22"
+
+// function halfQuarterEighth(n) {
+// 	let arr = []
+//     arr.push(n/2, n/4, n/8)
+//     return arr
+// }
+// console.log(halfQuarterEighth(6))
+
+'23'
+// function getMultipliedArr(arr) {
+// 	return arr.map((e) => e * 2)
+// }
+// console.log(getMultipliedArr([1, 86, -5]))
+
+'24'
+// function wordLengths(arr) {
+//     let arr1 = []
+// 	for(let i = 0; i < arr.length; i++){
+//          arr1.push(arr[i].length)
+//     }
+//     return arr1
+// }
+// console.log(wordLengths(["Halloween", "Thanksgiving", "Christmas"]))
+
+'25'
+
+// function length(s) {
+// 	return s.lastIndexOf('')
+// }
+// console.log(length("Hello world"))
+
+'26'
+
+// function spaceMeOut(str) {
+// 	return str.split("").join(" ")
+// }
+
+// console.log(spaceMeOut("space"))
+
+"27"
+
+// function getSumOfItems(arr) {
+// 	let str = 0
+//     for(let i = 0; i < arr.length; i ++){
+//         str += arr[i]
+//     }
+//     return str
+// }
+// console.log(getSumOfItems([2, 7, 4]))
+
+'28'
+
+// function invertArray(arr) {
+// 	return arr.map((e) => e * -1)
+// }
+// console.log(invertArray([1, 2, 3, 4, 5]))
+
+'29'
+
+// function evenOrOdd(arr) {
+// 	return arr.reduce((e, n) => (e + n), 0) ? 'odd' :'even' 
+// }
+// console.log(evenOrOdd([[0, 1, 5]]))
+
+'30'
+
+// function skipTooMuchSugarDrinks(drinks) {
+// 	return  drinks.filter((e) => e !== 'cola' && e !== 'fanta')
+// }
+// console.log(skipTooMuchSugarDrinks(["fanta", "cola", "water"]))
+
+// let arr=[1, 2, 3, 4, 5]
+
+// let poping = arr.pop()
+// arr.unshift(poping)
+// console.log(arr)
+
+// // function rotateByOne(arr) {
+// // 	// return arr
+// // }	
+// // console.log(rotateByOne([1, 2, 3, 4, 5]))
+
+
+// function asd(){
+    
+// }
+
+// const asd = () => {}
+
+// (function (){
+
+// }) ()
+
+
+let naming = "Dilshod   "
+let newname = naming.padEnd('bek', 0)
+console.log(newname)
